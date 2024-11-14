e5223054666af0c6da79d5d8b667998687e0e137
pekotte <golagar@hotmail.com>
2024-11-14 09:29:14 +0000

    Excel package ok, no open file

--- app.go
+++ app.go
@@ -1,42 +1,14 @@
 package main
 
 import (
-	"fmt"
+	//"fmt"
 
-	"github.com/xuri/excelize/v2"
-	//"excelHandler"
+	//"github.com/xuri/excelize/v2"
+
+	excelhandler "github.com/pekotte/goMigration/excelHandler"
 )
 
 func main() {
-	f, err := excelize.OpenFile("MyTool.xlsx")
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	defer func() {
-		if err := f.Close(); err != nil {
-			fmt.Println(err)
-		}
-	}()
-	// Obtener valor de la celda por el nombre y el eje de la hoja de trabajo dado.
-	/*cell, err := f.GetCellValue("Hoja1", "B2")
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	fmt.Println(cell)*/
-	// Obtener todas las filas en el Sheet1.
-	rows, err := f.GetRows("Hoja1")
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	for _, row := range rows {
-		for _, colCell := range row {
-			fmt.Print(colCell, "\t")
-			fmt.Print("  ||  ")
-		}
-		fmt.Println()
-		fmt.Println("======================================================================")
-	}
+
+	excelhandler.OpenAndShowCells("MyTool.xlsx", "Hoja1")
 }
--- excelHandler/index.excelHandler.go
+++ excelHandler/index.excelHandler.go
@@ -4,18 +4,17 @@ import (
 	"fmt"
 
 	"github.com/xuri/excelize/v2"
-	//"excelHandler"
 )
 
 func OpenAndShowCells(file string, sheet string) {
 	f, err := excelize.OpenFile(file)
 	if err != nil {
-		fmt.Println(err)
+		fmt.Println("(1) ", err)
 		return
 	}
 	defer func() {
 		if err := f.Close(); err != nil {
-			fmt.Println(err)
+			fmt.Println("(2) ", err)
 		}
 	}()
 	// Obtener valor de la celda por el nombre y el eje de la hoja de trabajo dado.
@@ -28,7 +27,7 @@ func OpenAndShowCells(file string, sheet string) {
 	// Obtener todas las filas en el Sheet1.
 	rows, err := f.GetRows(sheet)
 	if err != nil {
-		fmt.Println(err)
+		fmt.Println("(3) ", err)
 		return
 	}
 	for _, row := range rows {
