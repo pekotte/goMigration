36e9654324a4bfb32a188ebd651e856fe5dad24c
pekotte <golagar@hotmail.com>
2024-11-18 12:16:34 +0000

    GO - Paquete terminado

--- app.go
+++ app.go
@@ -11,22 +11,22 @@ func main() {
 	//excelhandler.OpenAndShowAllCells("MyTool.xlsx", "Hoja1")
 
 	type sEmployee struct {
-		ui32idAgresso uint32
+		strIdAgresso  string
 		strName       string
 		strDomain     string
 		strCmpyStDate string
 		strEmail      string
-		bPcAlten      bool
-		iOficina      int
+		strPcAlten    string
+		strOficina    string
 		strLocation   string
 		strLocAssoc   string
-		ui32Salary    uint32
-		ui32Bond      uint32
-		bRestaurant   bool
-		bInsurance    bool
-		ui8Holidays   uint8
+		strSalary     string
+		strBond       string
+		strRestaurant string
+		strInsurance  string
+		strHolidays   string
 		strComments   string
-		ui8SalaryDay  uint8
+		ui8SalaryDay  string
 		strBmADC      string
 		strCategory   string
 		strN1         string
@@ -46,10 +46,48 @@ func main() {
 		strName string
 	}
 
-	var newClient sClient = sClient{
-		excelhandler.OpenAndShowOneCells("MyTool.xlsx", "Hoja1", "D2"),
-		excelhandler.OpenAndShowOneCells("MyTool.xlsx", "Hoja1", "E2")}
+	rows := excelhandler.GetFileRows()
 
-	fmt.Println(newClient)
+	i := 2
+	for rows.Next() {
+		fmt.Println("------------------------------------------------")
+		var newClient sClient = sClient{
+			excelhandler.GetOneData(excelhandler.CLIENT_COD, i),
+			excelhandler.GetOneData(excelhandler.CLIENT_NAME, i)}
+		fmt.Println(newClient)
+
+		var newProyect sProyect = sProyect{
+			excelhandler.GetOneData(excelhandler.PROYECT_COD, i),
+			excelhandler.GetOneData(excelhandler.PROYECT_NAME, i),
+			excelhandler.GetOneData(excelhandler.PROYECT_STATUS, i),
+			excelhandler.GetOneData(excelhandler.PROYECT_SDATE, i)}
+		fmt.Println(newProyect)
+
+		var newEmployee sEmployee = sEmployee{
+			excelhandler.GetOneData(excelhandler.EMPLOYEE_AGRESSO, i),
+			excelhandler.GetOneData(excelhandler.EMPLOYEE_NAME, i),
+			excelhandler.GetOneData(excelhandler.EMPLOYEE_DOMAIN, i),
+			excelhandler.GetOneData(excelhandler.EMPLOYEE_CMPYSTDATE, i),
+			excelhandler.GetOneData(excelhandler.EMPLOYEE_EMAIL, i),
+			excelhandler.GetOneData(excelhandler.EMPLOYEE_PC_ALTEN, i),
+			excelhandler.GetOneData(excelhandler.EMPLOYEE_OFFICE, i),
+			excelhandler.GetOneData(excelhandler.EMPLOYEE_LOCATION, i),
+			excelhandler.GetOneData(excelhandler.EMPLOYEE_LOC_ASSOC, i),
+			excelhandler.GetOneData(excelhandler.EMPLOYEE_SALARY, i),
+			excelhandler.GetOneData(excelhandler.EMPLOYEE_BOND, i),
+			excelhandler.GetOneData(excelhandler.EMPLOYEE_RESTAURANT, i),
+			excelhandler.GetOneData(excelhandler.EMPLOYEE_INSURANCE, i),
+			excelhandler.GetOneData(excelhandler.EMPLOYEE_HOLIDAYS, i),
+			excelhandler.GetOneData(excelhandler.EMPLOYEE_COMMENTS, i),
+			excelhandler.GetOneData(excelhandler.EMPLOYEE_SALARY_DAY, i),
+			excelhandler.GetOneData(excelhandler.EMPLOYEE_BM_ADC, i),
+			excelhandler.GetOneData(excelhandler.EMPLOYEE_CATEGORY, i),
+			excelhandler.GetOneData(excelhandler.EMPLOYEE_N1, i),
+			excelhandler.GetOneData(excelhandler.EMPLOYEE_N2, i),
+			excelhandler.GetOneData(excelhandler.EMPLOYEE_INT_BM, i)}
+		fmt.Println(newEmployee)
+
+		i++
+	}
 
 }
--- excelHandler/index.excelHandler.go
+++ excelHandler/index.excelHandler.go
@@ -2,12 +2,47 @@ package excelhandler
 
 import (
 	"fmt"
+	"strconv"
 
 	"github.com/xuri/excelize/v2"
 )
 
-func OpenAndShowAllCells(file string, sheet string) {
-	f, err := excelize.OpenFile(file)
+var myFile string = "MyTool.xlsx"
+var mySheet string = "Hoja1"
+
+const (
+	CLIENT_COD     string = "D"
+	CLIENT_NAME    string = "E"
+	PROYECT_COD    string = "H"
+	PROYECT_NAME   string = "G"
+	PROYECT_STATUS string = "J"
+	PROYECT_SDATE  string = "L"
+
+	EMPLOYEE_AGRESSO    string = "A"
+	EMPLOYEE_NAME       string = "B"
+	EMPLOYEE_DOMAIN     string = "C"
+	EMPLOYEE_CMPYSTDATE string = "K"
+	EMPLOYEE_EMAIL      string = "M"
+	EMPLOYEE_PC_ALTEN   string = "N"
+	EMPLOYEE_OFFICE     string = "O"
+	EMPLOYEE_LOCATION   string = "P"
+	EMPLOYEE_LOC_ASSOC  string = "Q"
+	EMPLOYEE_SALARY     string = "R"
+	EMPLOYEE_BOND       string = "S"
+	EMPLOYEE_RESTAURANT string = "T"
+	EMPLOYEE_INSURANCE  string = "U"
+	EMPLOYEE_HOLIDAYS   string = "V"
+	EMPLOYEE_COMMENTS   string = "W"
+	EMPLOYEE_SALARY_DAY string = "X"
+	EMPLOYEE_BM_ADC     string = "Y"
+	EMPLOYEE_CATEGORY   string = "Z"
+	EMPLOYEE_N1         string = "AA"
+	EMPLOYEE_N2         string = "AB"
+	EMPLOYEE_INT_BM     string = "AC"
+)
+
+func ShowAllCells() {
+	f, err := excelize.OpenFile(myFile)
 	if err != nil {
 		fmt.Println("(1) ", err)
 		return
@@ -15,17 +50,11 @@ func OpenAndShowAllCells(file string, sheet string) {
 	defer func() {
 		if err := f.Close(); err != nil {
 			fmt.Println("(2) ", err)
+			return
 		}
 	}()
-	// Obtener valor de la celda por el nombre y el eje de la hoja de trabajo dado.
-	/*cell, err := f.GetCellValue("Hoja1", "B2")
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	fmt.Println(cell)*/
-	// Obtener todas las filas en el Sheet1.
-	rows, err := f.GetRows(sheet)
+
+	rows, err := f.GetRows(mySheet)
 	if err != nil {
 		fmt.Println("(3) ", err)
 		return
@@ -36,12 +65,34 @@ func OpenAndShowAllCells(file string, sheet string) {
 			fmt.Print("  ||  ")
 		}
 		fmt.Println()
-		fmt.Println("======================================================================")
 	}
 }
 
-func OpenAndShowOneCells(file string, sheet string, cell string) string {
-	f, err := excelize.OpenFile(file)
+func GetFileRows() *excelize.Rows {
+	f, err := excelize.OpenFile(myFile)
+	if err != nil {
+		fmt.Println("(1) ", err)
+		return nil
+	}
+	defer func() {
+		if err := f.Close(); err != nil {
+			fmt.Println("(2) ", err)
+			return
+		}
+	}()
+
+	rows, err := f.Rows(mySheet)
+	if err != nil {
+		fmt.Println(err)
+		return nil
+	}
+
+	return rows
+}
+
+func OpenAndShowOneCells(cell string) string {
+	// Obtener valor de la celda por el nombre y el eje de la hoja de trabajo dado.
+	f, err := excelize.OpenFile(myFile)
 	if err != nil {
 		fmt.Println("(1) ", err)
 		return ""
@@ -49,14 +100,18 @@ func OpenAndShowOneCells(file string, sheet string, cell string) string {
 	defer func() {
 		if err := f.Close(); err != nil {
 			fmt.Println("(2) ", err)
+			return
 		}
 	}()
-	// Obtener valor de la celda por el nombre y el eje de la hoja de trabajo dado.
-	mycell, err := f.GetCellValue(sheet, cell)
+
+	mycell, err := f.GetCellValue(mySheet, cell)
 	if err != nil {
 		fmt.Println(err)
 		return ""
 	}
-	//fmt.Println(mycell)
 	return mycell
+}
+
+func GetOneData(dataType string, row int) string {
+	return OpenAndShowOneCells(dataType + strconv.Itoa(row))
 }
